b95b65b5546bd31cca710f0a2123bc6c7643a299
Antoine <24255551+RedSkyi@users.noreply.github.com>
2022-05-17 16:46:59 -0400

    Setup theme colors

--- app/src/main/java/fr/antoinev/todoapp/ui/theme/Color.kt
+++ app/src/main/java/fr/antoinev/todoapp/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package fr.antoinev.todoapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val VeryDarkGrayishBlue = Color(0xFF2B2D42) // Background
+val DarkGrayishBlue = Color(0xFF8D99AE)
+val LightGrayishBlue = Color(0xFFEDF2F4)    // Primary
+val VividRed = Color(0xFFEF233C)            // Secondary
--- app/src/main/java/fr/antoinev/todoapp/ui/theme/Theme.kt
+++ app/src/main/java/fr/antoinev/todoapp/ui/theme/Theme.kt
@@ -1,39 +1,18 @@
 package fr.antoinev.todoapp.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    background = VeryDarkGrayishBlue,
+    primary = LightGrayishBlue,
+    secondary = VividRed
 )
 
 @Composable
-fun TodoAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+fun TodoAppTheme(content: @Composable () -> Unit) {
+    val colors = DarkColorPalette
 
     MaterialTheme(
         colors = colors,
