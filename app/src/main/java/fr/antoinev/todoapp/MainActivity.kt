30f863feff8783925b06b9dda2c7c3c26cca27a0
Antoine <24255551+RedSkyi@users.noreply.github.com>
2022-05-17 13:03:03 -0400

    Setup nav host and nav routes

--- app/src/main/java/fr/antoinev/todoapp/MainActivity.kt
+++ app/src/main/java/fr/antoinev/todoapp/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import fr.antoinev.todoapp.ui.theme.TodoAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +24,32 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    TodoAppScreen()
                 }
             }
         }
     }
 }
 
+sealed class Routes(val route: String) {
+    object Home : Routes("home")
+    object CreateTodo : Routes("todo_create")
+}
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TodoAppScreen() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Routes.Home.route) {
+        composable(Routes.Home.route) {  }
+        composable(Routes.CreateTodo.route) {  }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TodoAppTheme {
-        Greeting("Android")
+        TodoAppScreen()
     }
 }
